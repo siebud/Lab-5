c1963538be9ada16a6fedf73445beae1c441c477
Sierra Budinoff <sierra.j.budinoff-1@ou.edu>
2019-02-16 18:43:15 -0600

    Todo Estimation

---
+++ Todo.java
@@ -0,0 +1,28 @@
+public class Todo {/*
+Lab Section_________: 13
+Last Modified_______: 2019-02-16
+OU ID_______________: 113471919
+OU EMAIL____________: sierra.j.budinoff-1@ou.edu
+
+Tasks:
+1. parseAndExecute method
+2. write execute()
+3. declare three token methods variables
+4. write three tokens method equations
+5. declare two token methods variables
+6. write two tokens method equations
+7. finish calulatorTest cases
+
+Estimated Time (minutes):
+1. 20
+2. 20
+3. 15
+4. 20
+5. 15
+6. 20
+7. 30
+
+Actual Time (minutes):
+1. XXX
+2. XXX
+*/}
